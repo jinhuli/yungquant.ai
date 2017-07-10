3f0505a4fe9551c67dbe8c48af566976d2230103
Shane Kennedy <shaneikennedy@Shanes-MacBook-Pro.local>
2017-07-10 00:09:58 -0400

    make each client independant of eachother

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,23 +1,45 @@
+var id;
+
 $(function () {
     var socket = io.connect();
+    var welcome = 1;
     var $messageForm = $('#messageForm');
     var $message = $('#message');
-    var $chat = $('#chat');
+    var $chaß = $('#chat');
     var $messageBox = $('#messageBox');
+    id = guid();
+    if(welcome==1){
+        socket.emit('page ready', {id: id});
+        welcome = 0;
+    }
 
     $messageForm.submit(function (e) {
         e.preventDefault();
-        socket.emit('send message', {message: $message.val(), sender: "user"});
+        socket.emit('toYQ', {message: $message.val(), sender: "user", id: id});
         $message.val('');
     });
 
-    socket.on('new message', function (data) {
+    socket.on('forUser', function (data) {
         if(data.sender != "yq"){
-            $messageBox.append("<div class='well messageBubble' id='user' style='width:90%;'>" + data.msg + "</div>");
+            if(data.id == id){
+                $messageBox.append("<div class='well messageBubble' id='user' style='width:90%;'>" + data.msg + "</div>");
+            }
         }
-        else{
-            $messageBox.append("<div class='well messageBubble' id='bot' style='width:90%;'>" + data.msg + "</div>");
+        else{  
+            if(data.id == id){ 
+                $messageBox.append("<div class='well messageBubble' id='bot' style='width:90%;'>" + data.msg + "</div>");
+            }
         }
         $("#messageBox").animate({ scrollTop: $('#messageBox').prop("scrollHeight")}, 1000);
     })
 });
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
--- sockets/yq.js
+++ sockets/yq.js
@@ -7,7 +7,11 @@ var chat = function(io) {
     io.sockets.on('connection', function(socket){
         connections.push(socket);
         console.log('connected %s sockets connected', connections.length);
-        io.sockets.emit('new message', {msg: "Welcome to Yung Quant, your personal quantitative developer and analyst. If you're a regular, you know what to do. If you're a newbie ask for help!", sender: "yq"});
+
+        //initial message
+        socket.on('page ready', function(data){
+            io.sockets.emit('forUser', {msg: "Welcome to Yung Quant, your personal quantitative developer and analyst. If you're a regular, you know what to do. If you're a newbie ask for help!", sender: "yq", id: data.id});
+        });
 
 
         //Disconnect
@@ -17,25 +21,25 @@ var chat = function(io) {
         });
 
         //Send Message
-        socket.on('send message', function(data){
-            io.sockets.emit('new message', {msg: data.message, sender: "user"});
+        socket.on('toYQ', function(data){
+            io.sockets.emit('forUser', {msg: data.message, sender: data.sender, id: data.id});
             if(data.message == "Hi" || data.message == "hi" || data.message == "hey" || data.message == "Hey"){
-                io.sockets.emit('new message', {msg: "Hi! Let's make some money \\m/", sender: "yq"}); return;
+                io.sockets.emit('forUser', {msg: "Hi! Let's make some money \\m/", sender: "yq", id: data.id}); return;
             }
             if(data.message == "Help" || data.message == "help" || data.message == "Help me" || data.message == "help me"){
-                io.sockets.emit('new message', {msg: "First, make sure you've had a look at the Strategies page and know which strategy you're interested in. Once you've done that, find your strategy under the prompts heading to my left and send me a message with your strategy prompt with the universe that you're interested in trading", sender: "yq"});
-                io.sockets.emit('new message', {msg: "Here's and example: 'rev spdr' This will screen for stocks from the spdr ETFs using the mean reversion strategy", sender: "yq"});
+                io.sockets.emit('forUser', {msg: "First, make sure you've had a look at the Strategies page and know which strategy you're interested in. Once you've done that, find your strategy under the prompts heading to my left and send me a message with your strategy prompt with the universe that you're interested in trading", sender: "yq", id: data.id});
+                io.sockets.emit('forUser', {msg: "Here's and example: 'rev spdr' This will screen for stocks from the spdr ETFs using the mean reversion strategy", sender: "yq", id: data.id});
                 var helpOptions = {
                     args: "rev spdr",
                     pythonPath: "python3"
                 }
                 PythonShell.run('/python/controller.py', helpOptions, function(err, res){
-                    if(err) throw err;
+                    if(err) io.sockets.emit('forUser', {msg: 'Sorry! Something weird happened, could you send that again please? If the issue persists, please contact shane.kennedy19@gmail.com', sender: "yq", id: data.id});;
                     console.log(res);
-                    io.sockets.emit('new message', {msg: res[0], sender: "yq"});
-                    io.sockets.emit('new message', {msg: res[1], sender: "yq"});
-                    io.sockets.emit('new message', {msg: "As you can guess, those are the stocks that you would buy and sell using the mean reversion strategy. If you want to see the stock chart for any of those, look at the charts to my right and type in the symbol you want to see in the top left text box of any of those charts", sender: "yq"});
-                    io.sockets.emit('new message', {msg: "Hope that helps! Happy investing", sender: "yq"});
+                    io.sockets.emit('forUser', {msg: res[0], sender: "yq", id: data.id});
+                    io.sockets.emit('forUser', {msg: res[1], sender: "yq", id: data.id});
+                    io.sockets.emit('forUser', {msg: "As you can guess, those are the stocks that you would buy and sell using the mean reversion strategy. If you want to see the stock chart for any of those, look at the charts to my right and type in the symbol you want to see in the top left text box of any of those charts", sender: "yq", id: data.id});
+                    io.sockets.emit('forUser', {msg: "Hope that helps! Happy investing", sender: "yq", id: data.id});
                     return;
                 });    
             }
@@ -45,14 +49,14 @@ var chat = function(io) {
                     pythonPath: "python3"
                 }
                 PythonShell.run('/python/controller.py', options, function(err, res){
-                    if(err) throw err;
+                    if(err) io.sockets.emit('forUser', {msg: 'Sorry! Something weird happened, could you send that again please? If the issue persists, please contact shane.kennedy19@gmail.com', sender: "yq", id: data.id});
                     console.log(res);
                     if(res==null){
-                        io.sockets.emit('new message', {msg: 'Sorry! Make sure you\'ve given a proper prompt and universe!', sender: "yq"});
+                        io.sockets.emit('forUser', {msg: 'Sorry! Make sure you\'ve given a proper prompt and universe!', sender: "yq", id: data.id});
                     }
                     else{
-                        io.sockets.emit('new message', {msg: res[0], sender: "yq"});
-                        io.sockets.emit('new message', {msg: res[1], sender: "yq"});
+                        io.sockets.emit('forUser', {msg: res[0], sender: "yq", id: data.id});
+                        io.sockets.emit('forUser', {msg: res[1], sender: "yq", id: data.id});
                     }
                 });
             }
