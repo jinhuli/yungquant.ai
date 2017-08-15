2308c38def34d99e5c4bd8bf8215edf0f1f2babc
Shane Kennedy <shaneikennedy@Shanes-MacBook-Pro.local>
2017-08-15 10:37:21 -0400

    update favicon

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.engine('ejs', require('express-ejs-extend'));
 
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public/', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/favicon', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
