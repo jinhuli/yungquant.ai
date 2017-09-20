c04d090a709a1a8c5cf4416043734d5348bf1a14
Shane Kennedy <shaneikennedy@Shanes-MacBook-Pro.local>
2017-09-20 18:38:45 -0400

    clean up, add comments

--- sockets/yq.js
+++ sockets/yq.js
@@ -25,9 +25,11 @@ var chat = function(io) {
         //Send Message
         socket.on('toYQ', function(data){
             io.sockets.emit('forUser', {msg: data.message, sender: data.sender, id: data.id});
+            //stock greeting
             if(data.message == "Hi" || data.message == "hi" || data.message == "hey" || data.message == "Hey"){
                 io.sockets.emit('forUser', {msg: "Hi! Let's make some money \\m/", sender: "yq", id: data.id}); return;
             }
+            //user asks for help
             else if(data.message == "Help" || data.message == "help" || data.message == "Help me" || data.message == "help me"){
                 io.sockets.emit('forUser', {msg: "First, make sure you've had a look at the Strategies page and know which strategy you're interested in. Once you've done that, find your strategy under the prompts heading to my left and send me a message with your strategy prompt with the universe that you're interested in trading", sender: "yq", id: data.id});
                 io.sockets.emit('forUser', {msg: "Here's and example: 'mac spdr' This will screen for stocks from the spdr ETFs using the moving average crossover strategy", sender: "yq", id: data.id});
@@ -44,6 +46,7 @@ var chat = function(io) {
                     return;
                 });    
             }
+            //user wants to be reminded of prompts (mobile)
             else if(data.message == "prompts" || data.message == "Prompts"){
                 var prompts = "<li class='list-item'> Monte-Carlo Simulation: monte (symbol) </li>\
                         <li class='list-item'> Reversion to the mean: rev (universe) </li>\
@@ -53,12 +56,14 @@ var chat = function(io) {
                         <li class='list-item'> Sentiment Analysis: Coming Soon! </li>";
                 io.sockets.emit('forUser', {msg: prompts, sender: "yq", id: data.id});
             }
+            //user wants to be reminded of universes available (mobile)
             else if(data.message == "universes" || data.message == "Universes"){
                 var universes = "<li class='list-item'> S&P 100: sp100 </li>\
                         <li class='list-item'> NASDAQ 100: nas100 </li>\
                         <li class='list-item'> SPDR ETFs: spdr </li>";
                 io.sockets.emit('forUser', {msg: universes, sender: "yq", id: data.id});
             }
+            //user enters anything else, likely a proper prompt
             else{
                 
                 io.sockets.emit('forUser', {msg: "This could take up to 15 seconds...", sender: "yq", id: data.id});
