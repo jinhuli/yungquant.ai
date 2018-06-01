e86425b19cfd102cd7437587cc65797ec4e142d7
Shane Kennedy <shane.kennedy19@gmail.com>
2018-06-01 14:11:06 -0400

    switch back to prod

--- python/fetchHist.py
+++ python/fetchHist.py
@@ -1,5 +1,6 @@
 import pandas_datareader.data as web
 import datetime
+import requests
 
 def fetchHist(symbol):
 	year, month, day = str(datetime.date.today()).split('-')
@@ -7,3 +8,11 @@ def fetchHist(symbol):
 	end = datetime.datetime(int(year), int(month), int(day))
 	f = web.DataReader(symbol, 'yahoo', start, end)
 	return list(f['Adj Close'])[::-1]
+
+
+def new():
+	url = "https://api.robinhood.com/quotes/historicals/AAPL"
+	r = requests.get(url)
+	print(r.json)
+
+new()
