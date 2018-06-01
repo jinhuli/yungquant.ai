cb7355c370f6248891c21729bcc89a7f4e208d20
Shane Kennedy <shane.kennedy19@gmail.com>
2018-06-01 14:15:10 -0400

    fix fetchhist

--- python/fetchHist.py
+++ python/fetchHist.py
@@ -8,11 +8,3 @@ def fetchHist(symbol):
 	end = datetime.datetime(int(year), int(month), int(day))
 	f = web.DataReader(symbol, 'yahoo', start, end)
 	return list(f['Adj Close'])[::-1]
-
-
-def new():
-	url = "https://api.robinhood.com/quotes/historicals/AAPL"
-	r = requests.get(url)
-	print(r.json)
-
-new()
